72934d01375e51b846eb75a46ac6771ab68f73f3
MrKarimov01 <tungiborilar094@gmail.com>
2023-09-09 14:37:55 +0500

    Data qo'shildi

---
+++ src/Data/Data.js
@@ -0,0 +1,8 @@
+export const userProfile = [
+    {
+        id:"1",
+        fullName:"Nargiza Akhmedova",
+        profileImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5lr2uxXdbKkcJ0dGlev6nGlWlMMoy4pim5g&usqp=CAU",
+        ielts:"7"
+    },
+]
