64dcdd1bbc398442ef886534c72d8722d0f02302
MrKarimov01 <tungiborilar094@gmail.com>
2023-09-09 14:21:38 +0500

    Router dom va root color qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const App = () =>{
-  return(
-    <h1>App</h1>
-  )
-}
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>App</h1>
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
