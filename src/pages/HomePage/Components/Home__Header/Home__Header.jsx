0d2a2eee427c4d13b850afb7483ecc7ab6b170fe
XayitovLochinbek <xayitovlochinbek2008@gmail.com>
2023-09-13 20:14:47 +0500

    subjects

--- src/pages/HomePage/Components/Home__Header/Home__Header.jsx
+++ src/pages/HomePage/Components/Home__Header/Home__Header.jsx
@@ -279,6 +279,47 @@ const Home__Header = () => {
                         </div>
                     </div>
                 </div>
+                <div className="home__hero-inner-subjects">
+                    <p className="subject-p">Самые популярные факультеты</p>
+                </div>
+                <div className="home__hero-inner-subjects-subjects">
+                <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/ceaf/f8cc/0b162ffc4326639039c4c7f72b68ee83?Expires=1695600000&Signature=O9yijJ6bdUybUZDm~cH4pRAeCmkKKzCCG5D02Uj0Ts1dYt34cEknpxrwhR-efy~ikANhK4k5UbzvaZy5zpiQgTZKC5p3i-ungbDGK7pdZfd0yETZFXVn3JWy9gKy0NZDuZPD~jp4cOIS9VVnC6TpxWqaKiLD6OXWxU5JlzSqZ56an2qJQtCZxw8OfzQMxvzRW-L19oaezAg~QTmS9O4jmZlj1POvOzQ56VSn-3zUmepZKbaIdodrxTg4mz81DOwfVCbOsfT-L~NOxbuvepIU0BfL6Kg6aDWpCVZ~6K2tpfyteEtZ5kSJSX6X0JsKPjHqvs93ruwxHeQZHaSL9-8~yg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>Английский Язык</p>
+                    </div>
+                    <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/26da/89d1/fc5cba7ddcb7c4c02eaea7e93ca0000b?Expires=1695600000&Signature=B77R~C0ddJqUhc-f~2DEGa397F-jpwV1iR5uYtnHC9RjxdIFL3kWrFJpmbU2mrvgE-oLNWTauORzrjPSrfehcvqmzuKyOAYxT~v4SrBDjcbzF31bS8pigXNurXBA7EZjrRMy2ysYrbfT8UTYiQ8T9IV7BKn4Ptpr4bUfpJEAoGZrdEaYLgC5hPMMbs-04qMt0cMqTVks~RRIWuBQfzbj0y3IYd9ZLpRrDnUFXa81d8aMUQErqkb2EKfQZ-AsoRIvXl3p0HrLOyZ3H641mwa0r7Xx~tAAQQ-bWQ8bmx7xLG4QeAnOTA8M9bWM4XMV2WfPnR0OMlMrnkQWUNOpUKepJA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>Бизнес менеджмент</p>
+                    </div>
+                    <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/ad21/31ac/af59d7ca24bb77698839fce0915008bf?Expires=1695600000&Signature=IQZZddA7oeTO6nTXnzx3g9cW0qXY3BIkr70ROGrp1JBIpHwmxECF~ZMJfObQiEiy-nUnGJpZf5ffDe7VUTjy54Q9x9l7ErBTtHvlIHczCgTfHXsky94MAb7-5qU0yxVFTU8F0CRGjSr~V2gX0dG~mFeKZXJWSDRWMw6T5Hm6K1bEA9esYGC5cDpAsX9wB8~-iQYhj58-GZN87RLUVvZNozgd28lq7Nl4XCYT172Eomju2X0XmO6wivxi7HSLiI19WgM7iZCHhA7Sc3sARQRdr1LgodnswsgIGnyWqOjSV-Twor6hAaHmqT6Oyry1tokrYm7krnxxnemr6vSij1kKqA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>Французский язык</p>
+                    </div>
+                    <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/5344/9229/3216c5a4ac6ec7f5d8f1ec53141a9aa5?Expires=1695600000&Signature=EyImonJexmpNt0WzkakGhPBFvp5ux239tRd1Lzny0VqpEPMnPjaHaxeqAKFLjSppXm~FGPnJfMoFCJyCBX8xTNr6hAM8nBWucrMJygCBaXvBMLPwLI5I0KEZfVhnOph7y6t8WcQx9wO5FBIlRLS0edSpqMCNCSBn1niotjOh8vfbUI363l7rGJNtyEKOwHCvP8yeoG9qxJW0C4XtvdzJbgpWTZk6ZHaoMurRi3CZ0dJsT9R-8B~OWsbR0-G-KolWctQETDdFKHjSUftkm3g9GBPeYzxt7RP93SuWY~aP9xqkRrxrvFBuHq7-X~N3~vn6FC6fxTAifPN8~0EtNxXOIw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>Отельный бизнес</p>
+                    </div>
+                    <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/fa13/174b/fbba039ebfe9502453cc4d6113ffc56a?Expires=1695600000&Signature=aTjBK-2k5INYNhmHazA~1qq8S44qgDj5f9akiVz4obsQNM5ccVZZetuPSYyzKa-rzOOUCllMSDl7PSz9u-6yb15Kv2rHU9G8Epi5NuqXv4~QhysHbhhKWkN0fJJXa9yOiJt8h0IAj-wBqM2UxxGdrt909s~TkYpV91r8kd6F1bCxucV0-fFb8SfLn8k813S8MoYIL16p5TpTZ7Sz9uaPfqnNJObXF-T~KyGxvu2d~TcE~Z5kSUu0Ag93o8wFIF8PcrKeuXH9uaiYKVDbLHOrOjkXfLgK7uIvyePVTkQgtTS-JgAVXgt2VCp4yhjTOHBdOHnYWLMUHS4gAQMnqXjgqA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>IT</p>
+                    </div>
+                    <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/654a/7dd3/c1d110e96f277310a556fce286fc3c59?Expires=1695600000&Signature=T31L5BVwNsQ2tV2PIUfKSV19JzgyE~uUe5ICaSVkqpAnGcGne9xK574dYOtGI18-PIaigveHu7v5Aked5eDlYdYDuepTaFU3WfihalDb8bkJGHGDpVjx21eJVxl~eYeqHzgmOsio-JX7k3QG1rT5rt6zMMfre6xnyMdqlA37bHJfSkE92FMwyKV~DLwcPihdrTX4tWHFYBAoj4utskhZRevlr8St2lPMHYFwzL6CKWFqyhir8WiSNu2-Strwclgjb52uzt8A1mzpRMvroktivhUt9acfC2rAJJNSdfA-W-wquzjghdMUfuzmRDsw016RNCCQBq8mJEDAYG54AYx-sw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>Финансы</p>
+                    </div>
+                    <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/8295/29a9/5fccd94bac2841afff17e9d39f7726a1?Expires=1695600000&Signature=TpcN6jDyb6wqUze8xaAp66KiL1MODPedt8tr5c8xunMmZ8afAyu3SY1-3EdMCe-3FsVCBoInAE-W6xfepImTq5RiEW151Q0AiNFHUEHhIEZVwQEKJQ879FeOXbHrl6nSu6s75I1LzT41tDYHrlqIJe8-OI7lsLvWy0cTLp9YzDAfBmtos0R8FUVom4HAYNaDVDZtygLXMQJBaXB8Bndp5f5ROu0ZVslNxkep9j2nWfkXE~wl~gx~zGlo4Ph6kKOxzfgmrsM479MIAWhVblm6eJhmrLnu44RdeHBUuABSV2muou96HEO4LMxiYVTBBCU5ATK8pTiC1fvjzG9AIypM8w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>Дипломатия</p>
+                    </div>
+                    <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/d676/a0f9/e01e2b1ed22a918dbcffeca178c59064?Expires=1695600000&Signature=Yt0B2bNKZyxBJDMZ3yALMat8qzQ0BjdcKMnvuw9paWUyy1s~W33eBM0owqpDzKIwQionpc59Vin~AHXnLZ~~mGWltPbyFsjDGfEcNqBjKUgkpE6DcxMcmtSA9TzvFkM~9SOi1piDc5qK-4~wR1oeolI0KdArY3vaZGGMZRkL3P90dOjGb1-g8kmN4yn4XzmDSGTs3oyivcdSD4Tfgwq9~JL6~WQph9GTkqm~-Pa8nqLc77jrN9tPP674bGZI6qeTnELzK~SIJskXM-7Fx-TDgEh13okJkXjwOqey0CflD8h--Q5ebmAE4YxAB9SsIwtD9b181PB~hTO53shu4R9tiA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>Литература</p>
+                    </div>
+                    <div className="subject-images">
+                    <img src="https://s3-alpha-sig.figma.com/img/ad21/31ac/af59d7ca24bb77698839fce0915008bf?Expires=1695600000&Signature=IQZZddA7oeTO6nTXnzx3g9cW0qXY3BIkr70ROGrp1JBIpHwmxECF~ZMJfObQiEiy-nUnGJpZf5ffDe7VUTjy54Q9x9l7ErBTtHvlIHczCgTfHXsky94MAb7-5qU0yxVFTU8F0CRGjSr~V2gX0dG~mFeKZXJWSDRWMw6T5Hm6K1bEA9esYGC5cDpAsX9wB8~-iQYhj58-GZN87RLUVvZNozgd28lq7Nl4XCYT172Eomju2X0XmO6wivxi7HSLiI19WgM7iZCHhA7Sc3sARQRdr1LgodnswsgIGnyWqOjSV-Twor6hAaHmqT6Oyry1tokrYm7krnxxnemr6vSij1kKqA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <p>Французский язык</p>
+                    </div>
+                </div>
             </section>
         </>
 
