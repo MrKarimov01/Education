2c81c61f3d8a8aa5dd87189edc736cf3c4f0ba95
Dev-Gulzoda <qahhorovairodaxon@gmail.com>
2023-09-21 14:58:47 +0500

    student page

---
+++ src/pages/Student/pages/Menedjer__page/Menedjer__page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./Menedjer__page.css"
+
+const Menedjer__page = () => {
+  return(
+    <>
+    <div>
+      <p>Персональный менеджер</p>
+      <p>Nargiza Akhmedova</p>
+
+   <div className="Menedjer__page">
+     <p>
+      Наргиза Ахмедова
+     </p>
+    <p>Chief Recruiter, Education </p>
+     <p className="about">
+     Наргиза Ахмедова является ветераном в деле рекруитинга и подбора 
+     университетов. Она вам все досколнально объяснит и всему научит. Наргиза 
+     Ахмедова является ветераном в деле рекруитинга и подбора университетов. 
+     Она вам все досколнально объяснит и всему научит.
+     </p>
+   </div>
+   </div>
+    </>
+)
+} 
+
+export default Menedjer__page
---
+++ src/pages/Student/pages/Univer__single/Univer__single.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Univer__single.css"
+
+const Univer__single = () => {
+  return(
+    <>
+   <div className="umumiy">
+    <p className="univer_name">Университеты / Akdeniz Universiteti (Turkiya)</p>
+    <div>
+      
+        <p>Nargiza Akhmedova</p>
+    </div>
+     <div className="Univer__single">
+        <img className="./img/single_img.png" src="" alt="Picture" />
+     <p className="zayaf_text1">  Ваша заявка была отправлена университету. <br /> Ждём ответа </p> 
+     <p className="zayaf_text2">  Примерное время ожидания: 2 недели</p>
+     <span className="span_text">2 недели</span>
+     </div>
+     </div>
+    </>
+)
+} 
+
+export default Univer__single
