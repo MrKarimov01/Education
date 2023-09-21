06fe6aae3952474ea66de9a1170a2c8a4d14e546
Abrorbek09 <erkinboyevabror90@gmail.com>
2023-09-21 15:24:37 +0500

    StudentHome_qoshildi

---
+++ src/pages/Student/pages/Student_home/Student_home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./StudentHome.css"
+
+const StudentHome = () => {
+    return(
+        <>
+        <div>
+            <p className="hi">Savnfhk</p>
+        </div>
+        </>
+
+
+    )
+}
+
+export default StudentHome;
