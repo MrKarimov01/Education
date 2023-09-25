442f05943b39e58d510a1787a924f987af75466c
XayitovLochinbek <xayitovlochinbek2008@gmail.com>
2023-09-25 19:03:01 +0500

    Tugatdimdeyarli

--- src/pages/HomePage/pages/Home__Search/Home__Search.jsx
+++ src/pages/HomePage/pages/Home__Search/Home__Search.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import "./Home__Search.css"
 import Home__Header from '../../Components/Home__Header/Home__Header'
 import Home__Hero_data from '../../Components/Home__Hero-data/Home__Hero-data'
+import Home__Hero_inner_footer from '../../Components/Home__Hero-inner-footer/Home__Hero-inner-footer'
 
 const Home__Search = () => {
   return (
     <>
       <Home__Header/>
+      <Home__Hero_data/>
+      <Home__Hero_inner_footer/>
+      
     </>
     
   )
