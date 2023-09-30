bd744839d1c610e471cd662ed6487701535325e4
Dev-Gulzoda <qahhorovairodaxon@gmail.com>
2023-09-30 14:35:47 +0500

    student

--- src/pages/Student/pages/Menedjer__page/Menedjer__page.jsx
+++ src/pages/Student/pages/Menedjer__page/Menedjer__page.jsx
@@ -4,25 +4,45 @@ import "./Menedjer__page.css"
 
 const Menedjer__page = () => {
   return(
-    <>
-    <div>
-      <p>Персональный менеджер</p>
-      <p>Nargiza Akhmedova</p>
+      <div className="Menedjer__page">
 
-   <div className="Menedjer__page">
-     <p>
-      Наргиза Ахмедова
-     </p>
-    <p>Chief Recruiter, Education </p>
-     <p className="about">
-     Наргиза Ахмедова является ветераном в деле рекруитинга и подбора 
-     университетов. Она вам все досколнально объяснит и всему научит. Наргиза 
-     Ахмедова является ветераном в деле рекруитинга и подбора университетов. 
-     Она вам все досколнально объяснит и всему научит.
-     </p>
-   </div>
-   </div>
-    </>
+        <p className="pmenedjer">
+        Персональный менеджер
+        </p>
+
+        <div className="Menedjer__page2">
+           <div className="nimadir">
+              <div className="imgname">
+               <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5lr2uxXdbKkcJ0dGlev6nGlWlMMoy4pim5g&usqp=CAU" alt="Picture" />
+              </div>
+          
+              <div className="nameabout">
+                <p className="m_name">Наргиза Ахмедова</p> 
+                <p className="m_about"> Chief Recruiter, Education </p>
+              </div>
+            </div>
+      
+
+          <div className="Menedjer__page3">
+            <p className="about">
+             <span className="ism"> Наргиза Ахмедова </span> является ветераном в деле 
+             рекруитинга и подбора университетов. Она вам все досколнально
+             объяснит и всему научит. Наргиза Ахмедова является ветераном в 
+             деле рекруитинга и подбора университетов. Она вам все досколнально 
+             объяснит и всему научит. 
+            </p>
+          </div>
+         <div  className="numberemail">
+            <span className="number">
+              Номер телефона: <p className="numbe">+998 (99) 607 - 74 - 40</p>
+            </span>
+            <span className="email">
+              Email адрес: <p className="numbe">Nargiza.education@gmail.com</p>
+            </span>
+          </div>
+         </div>
+        </div>
+     
 )
 } 
 
